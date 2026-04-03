ccbbbabdb995cd957334a1713c225fa663b71d07
AKKIREDDY JYOTHIRMAI <jyothiakkireddy1903@gmail.com>
2026-04-03 21:19:07 +0530

    disable eslint for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
